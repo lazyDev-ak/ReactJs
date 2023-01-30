ed7125b7ad55d1c09b887ce1c7107b89e7c8fb0e
lazyDev-ak <keneankit01062000@gmail.com>
2023-01-30 12:39:03 +0530

    30/01/23

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-// console.log(module)
-var http=require('http')
-var server=http.createServer(function(req,res){
-    if(req.url==="/check"){
-        res.end("Hello from Server!!!")
-    }
+const express=require('express')
+const cors=require('cors')
+
+const app=express()
+
+// const corsOptions={
+//     origin:"http://localhost:3000"
+// }
+
+// app.use(cors(corsOptions))
+app.use(express.json())
+
+// return string
+app.get("/check",(req,res)=>{
+    res.send("Hello from Node Express Server!!!")
 })
 
-server.listen(8787,()=>{
-    console.log("Server Started!!!");
+
+const person={
+    name:"Peter",
+    city:"Mumbai"
+}
+
+const users=[{
+    name:"john",
+    city:"Pune"
+},{
+    name:"Peter",
+    city:"Mumbai"
+}]
+
+//return single object
+app.get("/singleObject",(req,res)=>{
+    res.send(person)
+})
+
+//return multiple objects/array
+app.get("/multiple",(req,res)=>{
+    res.send(users)
+})
+
+app.post("/send",(req,res)=>{
+    console.log(req.body);
+    res.send(req.body)
+})
+
+app.listen(8889,()=>{
+    console.log('Server Started on Port 8889');
 })
