d6f427b4a1ac19fbde0aab29bcdab40e121ecc04
lazyDev-ak <keneankit01062000@gmail.com>
2023-02-02 12:50:27 +0530

    02/02/22

--- src/ApiCall.js
+++ src/ApiCall.js
@@ -3,6 +3,12 @@ import React,{useEffect} from 'react'
 
 function ApiCall(){
 
+    const userObj={
+        firstName:'',
+        lastName:''
+    }
+
+    const[user,setUser]=React.useState(userObj)
     const[users,setUsers]=React.useState([])
 
     useEffect(()=>{
@@ -13,9 +19,35 @@ function ApiCall(){
         })
     })
 
+    const handleInput=(event)=>{
+        setUser({...user,[event.target.name]:event.target.value})
+    }
+
+    const saveData=()=>{
+        axios.post("http://localhost:8889/saveData",user)
+        .then(response=>response.data)
+        .then(res=>{
+            console.log("Data saved!!!");
+        })
+    }
+
     return(
         <div>
             <h1>API Call!!!</h1>
+
+            <form>
+                <label>FirstName : </label>
+                <input type="text" name="firstName" onChange={handleInput}/>
+
+                <label>LastName : </label>
+                <input type="text" name="lastName" onChange={handleInput}/>
+
+                <label>City : </label>
+                <input type="text" name="city" onChange={handleInput}/>
+
+                <button type='button' onClick={saveData}>Submit</button>
+            </form>
+
             <ul>
                 {
                     users.map((user)=>(
